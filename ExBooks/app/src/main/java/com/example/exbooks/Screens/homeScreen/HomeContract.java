041c155c82be117bbb0ca82f859a7ef161752118
Esraa <esraahamed1122@gmail.com>
2019-04-12 18:46:44 +0200

    Home Activity Without Design

---
+++ ExBooks/app/src/main/java/com/example/exbooks/Screens/homeScreen/HomeContract.java
@@ -0,0 +1,12 @@
+package com.example.exbooks.Screens.homeScreen;
+
+public interface HomeContract {
+    interface HomeViewInterface
+    {
+
+    }
+    interface HomePresenterInterface
+    {
+
+    }
+}
