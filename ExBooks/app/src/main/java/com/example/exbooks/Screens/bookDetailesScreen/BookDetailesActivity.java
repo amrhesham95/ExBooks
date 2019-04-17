39f2e2abf00b266811ecd5424e26df9cad1a2ce9
Sahar Hany <sahar96hany@gmail.com>
2019-04-17 11:43:13 +0200

    handling call btn

--- ExBooks/app/src/main/java/com/example/exbooks/Screens/bookDetailesScreen/BookDetailesActivity.java
+++ ExBooks/app/src/main/java/com/example/exbooks/Screens/bookDetailesScreen/BookDetailesActivity.java
@@ -1,7 +1,11 @@
 package com.example.exbooks.Screens.bookDetailesScreen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,6 +33,15 @@ public class BookDetailesActivity extends AppCompatActivity implements BookDetai
         bookLocation=(TextView)findViewById(R.id.detailesBookLocation);
         bookImgView = findViewById(R.id.bookImageInDetailesActivity);
         setDataOfBook();
+        Button callBtn = findViewById(R.id.call_btn);
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01121668135"));
+                startActivity(intent);
+            }
+        });
 
     }
 
