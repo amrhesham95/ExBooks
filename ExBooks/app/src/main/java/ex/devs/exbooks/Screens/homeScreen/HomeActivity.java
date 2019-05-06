9e05bc37912ce7eb4d2b5b009f2bf20f48dac653
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-05-06 21:22:58 +0200

    add server notifcation

--- ExBooks/app/src/main/java/ex/devs/exbooks/Screens/homeScreen/HomeActivity.java
+++ ExBooks/app/src/main/java/ex/devs/exbooks/Screens/homeScreen/HomeActivity.java
@@ -2,11 +2,13 @@ package ex.devs.exbooks.Screens.homeScreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,13 +19,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ex.devs.exbooks.R;
 import ex.devs.exbooks.Screens.BookAddingScreen.BookAddingActivity;
 import ex.devs.exbooks.Screens.booksOfCategoryScreen.BooksOfCategoryActivity;
 import ex.devs.exbooks.Screens.chatHistoryScreen.ChatsActivity;
 import ex.devs.exbooks.Screens.myBooksScreen.MyBooksActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,HomeContract.HomeViewInterface  {
@@ -34,6 +41,7 @@ public class HomeActivity extends AppCompatActivity
     private CollapsingToolbarLayout mCollapsingToolbarLayout;
     private Toolbar mToolbar;
     TextView navigationViewHeader;
+    public static final String TOPIC_TAG="topic";
     //test
 
 
@@ -42,7 +50,18 @@ public class HomeActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        FirebaseMessaging.getInstance().subscribeToTopic("firstTopic")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = getString(R.string.topic);
+                        if (!task.isSuccessful()) {
+                            msg = getString(R.string.topicFailed);
+                        }
+                        Log.d(TOPIC_TAG, msg);
+                        Toast.makeText(HomeActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         mCollapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing);
         mToolbar=(Toolbar)findViewById(R.id.toolbar);
