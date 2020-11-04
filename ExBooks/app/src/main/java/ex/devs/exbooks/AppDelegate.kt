5667d041a163cb1a70de09ca95562802e3ba4877
emadahmed-vl <eahmed@victorylink.com>
2020-11-04 17:12:38 +0200

    Adding Hilt Dependency Injection Library

--- ExBooks/app/src/main/java/ex/devs/exbooks/AppDelegate.kt
+++ ExBooks/app/src/main/java/ex/devs/exbooks/AppDelegate.kt
@@ -3,7 +3,9 @@ package ex.devs.exbooks
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class AppDelegate : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
